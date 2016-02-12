a22ddad2ee63c8d445473b0531448bdf20f3d371
Jean Sipren <Spirou4D@users.noreply.github.com>
2016-02-12 16:07:42 +0100

    update_002
    corrected space and comments.

--- __init__.py
+++ __init__.py
@@ -38,6 +38,44 @@ Modif: 2016-02'01 Patrick optimize the code
 '''
 
 
+#-------------------------------------------------reload image
+class ImageReload(bpy.types.Operator):
+    """Reload Image Last Saved State"""
+    bl_idname = "image.reload_saved_state"
+    bl_label = "Reload Image Save Point"
+    bl_options = { 'REGISTER', 'UNDO' }
+
+
+    def execute(self, context):
+        scene = context.scene
+        original_type = bpy.context.area.type
+        bpy.context.area.type = 'IMAGE_EDITOR'
+
+        #return image to last saved state
+        bpy.ops.image.reload()
+        bpy.context.area.type = original_type
+        return {'FINISHED'}
+
+
+#-------------------------------------------------image save
+class SaveImage(bpy.types.Operator):
+    """Save Image"""
+    bl_idname = "image.save_current"
+    bl_label = "Save Image Current"
+    bl_options = { 'REGISTER', 'UNDO' }
+
+
+    def execute(self, context):
+        scene = context.scene
+        original_type = bpy.context.area.type
+        bpy.context.area.type = 'IMAGE_EDITOR'
+
+        #return image to last saved state
+        bpy.ops.image.save_dirty()
+        bpy.context.area.type = original_type
+        return {'FINISHED'}
+
+
 #--------------------------------------------------Create brush
 class BrushMakerScene(bpy.types.Operator):
     """Create Brush Scene"""
@@ -128,17 +166,106 @@ class CanvasShadeless(bpy.types.Operator):
         return {'FINISHED'}
 
 
+#-------------------------------------------------cameraview paint
+class CameraviewPaint(bpy.types.Operator):
+    """Create a front-of camera in painting mode"""
+    bl_idname = "image.cameraview_paint"
+    bl_label = "Cameraview Paint"
+    bl_options = {'REGISTER', 'UNDO'}
+
+
+    def execute(self, context):
+        #toggle on/off textpaint
+        obj = context.active_object
+
+        if obj:
+            mode = obj.mode
+            if mode == 'TEXTURE_PAINT':
+                bpy.ops.paint.texture_paint_toggle()
+
+        #save selected plane by rename
+        context.object.name = "canvas"
+
+        #variable to get image texture dimensions -
+        #thanks to Mutant Bob
+        #http://blender.stackexchange.com/users/660/mutant-bob
+        select_mat = obj.data.materials[0].texture_slots[0].\
+                    texture.image.size[:]
+
+        #add camera
+        bpy.ops.object.camera_add(view_align=False,
+                    enter_editmode=False,
+                    location=(0, 0, 0),
+                    rotation=(0, 0, 0),
+                    layers=(True, False, False, False, False, False,
+                    False, False,False, False, False, False, False,
+                    False, False, False, False,False, False, False))
+
+        #ratio full
+        bpy.context.scene.render.resolution_percentage = 100
+
+        #name it
+        context.object.name = "Canvas View Paint"
+
+        #switch to camera view
+        bpy.ops.view3d.object_as_camera()
+
+        #ortho view on current camera
+        context.object.data.type = 'ORTHO'
+
+        #move cam up in Z by 1 unit
+        bpy.ops.transform.translate(value=(0, 0, 1),
+                    constraint_axis=(False, False, True),
+                    constraint_orientation='GLOBAL',
+                    mirror=False,
+                    proportional='DISABLED',
+                    proportional_edit_falloff='SMOOTH',
+                    proportional_size=1)
+
+        #switch on composition guides for use in cameraview paint
+        context.object.data.show_guide = {'CENTER',
+                            'CENTER_DIAGONAL', 'THIRDS', 'GOLDEN',
+                            'GOLDEN_TRIANGLE_A', 'GOLDEN_TRIANGLE_B',
+                            'HARMONY_TRIANGLE_A', 'HARMONY_TRIANGLE_B'
+                            }
+
+        #found on net Atom wrote this simple script
+        #image_index = 0
+        rnd = bpy.data.scenes[0].render
+        rnd.resolution_x, rnd.resolution_y = select_mat
+
+        #resolution
+        rndx = rnd.resolution_x
+        rndy = rnd.resolution_y
+
+        #orthoscale = ((rndx - rndy)/rndy)+1
+        if rndx >= rndy:
+            orthoscale = ((rndx - rndy)/rndy)+1
+        elif rndx < rndy:
+            orthoscale = 1
+        context.object.data.ortho_scale = orthoscale
+        context.selectable_objects
+
+        #deselect camera
+        bpy.ops.object.select_all(action='TOGGLE')
+
+        #select plane
+        bpy.ops.object.select_all(action='DESELECT')
+        ob = bpy.data.objects["canvas"]
+        ob.select = True
+        context.scene.objects.active = ob
+
+        #selection to texpaint toggle
+        bpy.ops.paint.texture_paint_toggle()
+        return {'FINISHED'}
+
+
 #--------------------------------------------------flip  horiz. macro
 class CanvasHoriz(bpy.types.Operator):
     """Canvas Flip Horizontal Macro"""
-     # must match a operator context, like
-     # view3d, object or image and cannot have
-     # more then one '.', if you need something
-     # that is global use wm.create_brush
-     # and uncomment from line 24-29
     bl_idname = "image.canvas_horizontal"
     bl_label = "Canvas horiz"
-    bl_options = { 'REGISTER', 'UNDO' }
+    bl_options = {'REGISTER', 'UNDO'}
 
 
     def execute(self, context):
@@ -164,14 +291,9 @@ class CanvasHoriz(bpy.types.Operator):
 #--------------------------------------------------flip vertical macro
 class CanvasVertical(bpy.types.Operator):
     """Canvas Flip Vertical Macro"""
-     # must match a operator context, like
-     # view3d, object or image and cannot have
-     # more then one '.', if you need something
-     # that is global use wm.create_brush
-     # and uncomment from line 24-29
     bl_idname = "image.canvas_vertical"
     bl_label = "Canvas Vertical"
-    bl_options = { 'REGISTER', 'UNDO' }
+    bl_options = {'REGISTER', 'UNDO'}
 
 
     def execute(self, context):
@@ -198,14 +320,9 @@ class CanvasVertical(bpy.types.Operator):
 #-------------------------------------------------ccw15
 class RotateCanvasCCW15(bpy.types.Operator):
     """Image Rotate CounterClockwise 15 Macro"""
-     # must match a operator context, like
-     # view3d, object or image and cannot have
-     # more then one '.', if you need something
-     # that is global use wm.create_brush
-     # and uncomment from line 24-29
     bl_idname = "image.rotate_ccw_15"
     bl_label = "Canvas Rotate CounterClockwise 15"
-    bl_options = { 'REGISTER', 'UNDO' }
+    bl_options = {'REGISTER', 'UNDO'}
 
 
     def execute(self, context):
@@ -233,14 +350,9 @@ class RotateCanvasCCW15(bpy.types.Operator):
 #-------------------------------------------------cw15
 class RotateCanvasCW15(bpy.types.Operator):
     """Image Rotate Clockwise 15 Macro"""
-     # must match a operator context, like
-     # view3d, object or image and cannot have
-     # more then one '.', if you need something
-     # that is global use wm.create_brush
-     # and uncomment from line 24-29
     bl_idname = "image.rotate_cw_15"
     bl_label = "Canvas Rotate Clockwise 15"
-    bl_options = { 'REGISTER', 'UNDO' }
+    bl_options = {'REGISTER', 'UNDO'}
 
 
     def execute(self, context):
@@ -268,14 +380,9 @@ class RotateCanvasCW15(bpy.types.Operator):
 #-------------------------------------------------ccw 90
 class RotateCanvasCCW(bpy.types.Operator):
     """Image Rotate CounterClockwise 90 Macro"""
-     # must match a operator context, like
-     # view3d, object or image and cannot have
-     # more then one '.', if you need something
-     # that is global use wm.create_brush
-     # and uncomment from line 24-29
     bl_idname = "image.rotate_ccw_90"
     bl_label = "Canvas Rotate CounterClockwise 90"
-    bl_options = { 'REGISTER', 'UNDO' }
+    bl_options = {'REGISTER', 'UNDO'}
 
 
     def execute(self, context):
@@ -303,14 +410,9 @@ class RotateCanvasCCW(bpy.types.Operator):
 #-------------------------------------------------cw 90
 class RotateCanvasCW(bpy.types.Operator):
     """Image Rotate Clockwise 90 Macro"""
-     # must match a operator context, like
-     # view3d, object or image and cannot have
-     # more then one '.', if you need something
-     # that is global use wm.create_brush
-     # and uncomment from line 24-29
     bl_idname = "image.rotate_cw_90"
     bl_label = "Canvas Rotate Clockwise 90"
-    bl_options = { 'REGISTER', 'UNDO' }
+    bl_options = {'REGISTER', 'UNDO'}
 
 
     def execute(self, context):
@@ -335,36 +437,13 @@ class RotateCanvasCW(bpy.types.Operator):
         return {'FINISHED'}
 
 
-#-------------------------------------------------reload image
-class ImageReload(bpy.types.Operator):
-    """Reload Image Last Saved State"""
-    bl_idname = "image.reload_saved_state"
-    bl_label = "Reload Image Save Point"
-    bl_options = { 'REGISTER', 'UNDO' }
-
-
-    def execute(self, context):
-        scene = context.scene
-        original_type = bpy.context.area.type
-        bpy.context.area.type = 'IMAGE_EDITOR'
-
-        #return image to last saved state
-        bpy.ops.image.reload()
-        bpy.context.area.type = original_type
-        return {'FINISHED'}
-
 
 #-------------------------------------------------image rotation reset
 class CanvasResetrot(bpy.types.Operator):
     """Canvas Rotation Reset Macro"""
-     # must match a operator context, like
-     # view3d, object or image and cannot have
-     # more then one '.', if you need something
-     # that is global use wm.create_brush
-     # and uncomment from line 24-29
     bl_idname = "image.canvas_resetrot"
     bl_label = "Canvas Reset Rotation"
-    bl_options = { 'REGISTER', 'UNDO' }
+    bl_options = {'REGISTER', 'UNDO'}
 
 
     def execute(self, context):
@@ -374,126 +453,6 @@ class CanvasResetrot(bpy.types.Operator):
         bpy.ops.object.rotation_clear()
         return {'FINISHED'}
 
-
-#-------------------------------------------------cameraview paint
-class CameraviewPaint(bpy.types.Operator):
-    # must match a operator context, like
-    # view3d, object or image and cannot have
-    # more then one '.', if you need something
-    # that is global use wm.create_brush
-    # and uncomment from line 24-29
-    bl_idname = "image.cameraview_paint"
-    bl_label = "Cameraview Paint"
-    bl_options = { 'REGISTER', 'UNDO' }
-
-
-
-    def execute(self, context):
-        scene = context.scene
-
-        #toggle on/off textpaint
-        obj = context.active_object
-
-        if obj:
-            mode = obj.mode
-            # aslkjdaslkdjasdas
-            if mode == 'TEXTURE_PAINT':
-                bpy.ops.paint.texture_paint_toggle()
-
-        #save selected plane by rename
-        context.object.name = "canvas"
-
-        #variable to get image texture dimensions -
-        #thanks to Mutant Bob
-        #http://blender.stackexchange.com/users/660/mutant-bob
-        select_mat = obj.data.materials[0].texture_slots[0].texture.image.size[:]
-
-
-        #add camera
-        bpy.ops.object.camera_add(view_align=False,
-                    enter_editmode=False,
-                    location=(0, 0, 0),
-                    rotation=(0, 0, 0),
-                    layers=(True, False, False, False, False, False,
-                    False, False,False, False, False, False, False,
-                    False, False, False, False,False, False, False))
-
-        #ratio full
-        bpy.context.scene.render.resolution_percentage = 100
-
-        #name it
-        bpy.context.object.name = "Canvas View Paint"
-
-        #switch to camera view
-        bpy.ops.view3d.object_as_camera()
-
-        #ortho view on current camera
-        bpy.context.object.data.type = 'ORTHO'
-
-        #move cam up in Z by 1 unit
-        bpy.ops.transform.translate(value=(0, 0, 1),
-                    constraint_axis=(False, False, True),
-                    constraint_orientation='GLOBAL',
-                    mirror=False,
-                    proportional='DISABLED',
-                    proportional_edit_falloff='SMOOTH',
-                    proportional_size=1)
-
-        #switch on composition guides for use in cameraview paint
-        bpy.context.object.data.show_guide = {'CENTER',
-                            'CENTER_DIAGONAL', 'THIRDS', 'GOLDEN',
-                            'GOLDEN_TRIANGLE_A', 'GOLDEN_TRIANGLE_B',
-                            'HARMONY_TRIANGLE_A', 'HARMONY_TRIANGLE_B'
-                            }
-
-        #found on net Atom wrote this simple script
-        #image_index = 0
-        rnd = bpy.data.scenes[0].render
-        rnd.resolution_x, rnd.resolution_y = select_mat
-
-        #bpy.context.object.data.ortho_scale = orthoscale
-        rndx = rnd.resolution_x
-        rndy = rnd.resolution_y
-
-        #orthoscale = ((rndx - rndy)/rndy)+1
-        if rndx >= rndy:
-            orthoscale = ((rndx - rndy)/rndy)+1
-        elif rndx < rndy:
-            orthoscale = 1
-        bpy.context.object.data.ortho_scale = orthoscale
-        bpy.context.selectable_objects
-
-        #deselect camera
-        bpy.ops.object.select_all(action='TOGGLE')
-
-        #select plane
-        bpy.ops.object.select_all(action='DESELECT')
-        ob = bpy.data.objects["canvas"]
-        ob.select = True
-        bpy.context.scene.objects.active = ob
-
-        #selection to texpaint toggle
-        bpy.ops.paint.texture_paint_toggle()
-        return {'FINISHED'}
-
-
-#-------------------------------------------------image save
-class SaveImage(bpy.types.Operator):
-    """Save Image"""
-    bl_idname = "image.save_current"
-    bl_label = "Save Image Current"
-    bl_options = { 'REGISTER', 'UNDO' }
-
-
-    def execute(self, context):
-        scene = context.scene
-        original_type = bpy.context.area.type
-        bpy.context.area.type = 'IMAGE_EDITOR'
-
-        #return image to last saved state
-        bpy.ops.image.save_dirty()
-        bpy.context.area.type = original_type
-        return {'FINISHED'}
 
 
 
@@ -513,7 +472,7 @@ class ArtistPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
-        row = layout.row()
+        row = layout.row()  #INIT
         row.label(text="Image State")
         row = layout.row()
         row.operator("import_image.to_plane",
@@ -526,19 +485,21 @@ class ArtistPanel(bpy.types.Panel):
                     text = "Save Image", icon = 'IMAGEFILE')
         row = layout.row()
         row.operator("scene.create_brush_scene",
-                text="Create Brush Maker Scene", icon='OUTLINER_OB_CAMERA')
+                text="Create Brush Maker Scene",
+                icon='OUTLINER_OB_CAMERA')
 
 
-        row = layout.row()
+        row = layout.row()  #MACRO
         row.label(text="Special Macros")
         row = layout.row()
         row.operator("image.canvas_shadeless",
                     text = "Shadeless Canvas", icon = 'FORCE_TEXTURE')
         row = layout.row()
         row.operator("image.cameraview_paint",
-                    text = "Add Painting Camera", icon = 'RENDER_REGION')
+                    text = "Add Painting Camera",
+                    icon = 'RENDER_REGION')
 
-        row = layout.row()
+        row = layout.row()  #FLIP
         row.label(text="Flip")
         row = layout.row()
         row.operator("image.canvas_horizontal",
@@ -546,9 +507,10 @@ class ArtistPanel(bpy.types.Panel):
                     icon='ARROW_LEFTRIGHT')
         row = layout.row()
         row.operator("image.canvas_vertical",
-                    text = "Canvas Flip Vertical", icon = 'FILE_PARENT')
+                    text = "Canvas Flip Vertical",
+                    icon = 'FILE_PARENT')
 
-        row = layout.row()
+        row = layout.row()  #ROTATION
         row.label(text="Rotation")
         row = layout.row()
         row.operator("image.rotate_ccw_15",
@@ -569,34 +531,34 @@ class ArtistPanel(bpy.types.Panel):
 
 
 def register():
+    bpy.utils.register_class(ImageReload)
+    bpy.utils.register_class(SaveImage)
     bpy.utils.register_class(BrushMakerScene)
     bpy.utils.register_class(CanvasShadeless)
+    bpy.utils.register_class(CameraviewPaint)
     bpy.utils.register_class(CanvasHoriz)
     bpy.utils.register_class(CanvasVertical)
     bpy.utils.register_class(RotateCanvasCCW15)
     bpy.utils.register_class(RotateCanvasCW15)
     bpy.utils.register_class(RotateCanvasCCW)
     bpy.utils.register_class(RotateCanvasCW)
-    bpy.utils.register_class(ImageReload)
     bpy.utils.register_class(CanvasResetrot)
-    bpy.utils.register_class(CameraviewPaint)
-    bpy.utils.register_class(SaveImage)
     bpy.utils.register_class(ArtistPanel)
 
 def unregister():
     bpy.utils.unregister_class(ArtistPanel)
-    bpy.utils.unregister_class(SaveImage)
-    bpy.utils.unregister_class(CameraviewPaint)
     bpy.utils.unregister_class(CanvasResetrot)
-    bpy.utils.unregister_class(ImageReload)
     bpy.utils.unregister_class(RotateCanvasCW)
     bpy.utils.unregister_class(RotateCanvasCCW)
     bpy.utils.unregister_class(RotateCanvasCW15)
     bpy.utils.unregister_class(RotateCanvasCCW15)
     bpy.utils.unregister_class(CanvasVertical)
     bpy.utils.unregister_class(CanvasHoriz)
+    bpy.utils.unregister_class(CameraviewPaint)
     bpy.utils.unregister_class(CanvasShadeless)
     bpy.utils.unregister_class(BrushMakerScene)
+    bpy.utils.unregister_class(SaveImage)
+    bpy.utils.unregister_class(ImageReload)
 
 if __name__ == "__main__":
     register()
